51485688f44c188bb599d5ba4db2824733790563
kang0221 <1172106013@qq.com>
2021-03-08 11:44:51 +0800

    2

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 (async () => {
-  String.prototype.replaceWithMask = function (start, end) {
-    return this.substr(0, start) + '******' + this.substr(-end, end)
-  }
   require('./AutoSignMachine.js').run()
 })()
