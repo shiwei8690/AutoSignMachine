39ce5b2c43243df55155dd2751e3eb7ee94b9802
shiwei8690 <37175309+shiwei8690@users.noreply.github.com>
2021-04-18 04:37:17 +0800

    Update producGame.js

--- commands/tasks/unicom/producGame.js
+++ commands/tasks/unicom/producGame.js
@@ -364,8 +364,7 @@ var producGame = {
     },
     doGameFlowTask: async (axios, options) => {
         let { popularList: allgames, jar } = await producGame.popularGames(axios, options)
-        let games = await producGame.timeTaskQuery(axios, options)
-        games = allgames.filter(g => games.filter(g => g.state === '0').map(i => i.gameId).indexOf(g.id) !== -1)
+        let games = allgames.filter(g => g.state === ‘0‘)
         console.info('剩余未完成game', games.length)
         let queue = new PQueue({ concurrency: 15 });
 
